54b00af4ba14325361ea76274ea752ece4f6bc82
Sofía Vits Contreras <s.vitsc@udd.cl>
2025-08-06 18:01:11 -0400

    Primera versión main.py

---
+++ main.py
@@ -0,0 +1,62 @@
+import joblib
+import numpy as np
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+heart_model = joblib.load("./model/heart_model.joblib")
+
+def predict_heart_disease(features_patient, confidence):
+    """Recibe un vector de características de un paciente y predice si padece o no una enfermedad cardíaca.
+
+    Argumentos:
+        features_trip (array): Características del viaje, vector de tamaño 14.
+        confidence (float, opcional): Nivel de confianza. Por defecto es 0.5.
+    """
+
+    pred_value = heart_model.predict_proba(features_patient.reshape(1, -1))[0][1]
+    if pred_value >= confidence:
+      return 1
+    else:
+      return 0
+
+# Asignamos una instancia de la clase FastAPI a la variable "app".
+# Interacturaremos con la API usando este elemento.
+app = FastAPI(title='Implementando un modelo de predicción de presencia de enfermedades cardíacas usando FastAPI')
+
+# Creamos una clase para el vector de features de entrada
+class Item(BaseModel):
+    age: int
+    sex: int
+    cp:  int
+    trestbps: int
+    chol: int
+    fbs:  int
+    restecg: int
+    thalach: int
+    exang: int
+    oldpeak: float
+    slope: int
+    ca: int
+    thal: int
+
+# Usando @app.get("/") definimos un método GET para el endpoint / (que sería como el "home").
+@app.get("/")
+def home():
+    return "¡Felicitaciones! Tu API está funcionando según lo esperado. Anda ahora a http://localhost:8000/docs."
+
+
+# Este endpoint maneja la lógica necesaria para clasificar.
+# Requiere como entrada el vector de características del viaje y el umbral de confianza para la clasificación.
+@app.post("/predict")
+def prediction(item: Item, confidence: float):
+
+
+    # 1. Correr el modelo de clasificación
+    features_patient = np.array([item.age, item.sex, item.cp, item.trestbps, item. chol, item.fbs,
+                    item.restecg, item.thalach, item.exang, item.oldpeak, item.slope, item.ca, item.thal])
+    pred = predict_heart_disease(features_patient, confidence)
+
+    # 2. Transmitir la respuesta de vuelta al cliente
+
+    # Retornar el resultado de la predicción
+    return {'predicted_class': pred}
