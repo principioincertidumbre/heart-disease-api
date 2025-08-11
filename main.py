a48215fda0857699e01668623b6f4938e2e46d4d
Sofía Vits Contreras <s.vitsc@udd.cl>
2025-08-11 13:39:52 -0400

    Elimina referencia a localhost

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ class Item(BaseModel):
 # Usando @app.get("/") definimos un método GET para el endpoint / (que sería como el "home").
 @app.get("/")
 def home():
-    return "¡Felicitaciones! Tu API está funcionando según lo esperado. Anda ahora a http://localhost:8000/docs."
+    return "¡Felicitaciones! Tu API está funcionando según lo esperado."
 
 
 # Este endpoint maneja la lógica necesaria para clasificar.
